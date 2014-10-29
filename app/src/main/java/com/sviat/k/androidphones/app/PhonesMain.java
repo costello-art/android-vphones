7b030a3f3fbe2520ac413836445242155786e850
Sviat Costigan <costello@users.noreply.github.com>
2014-10-29 20:55:28 +0200

    starting from scratch

--- app/src/main/java/com/sviat/k/androidphones/app/PhonesMain.java
+++ app/src/main/java/com/sviat/k/androidphones/app/PhonesMain.java
@@ -1,25 +1,16 @@
 package com.sviat.k.androidphones.app;
 
-import android.app.LoaderManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v4.content.CursorLoader;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 
-public class PhonesMain extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener {
+public class PhonesMain extends ActionBarActivity {
 
     public static final String EXTRA_MESSAGE_TO_RECEIVE = "com.sviat.k.androidphones.app.message_to_send";
 
@@ -30,7 +21,7 @@ public class PhonesMain extends ActionBarActivity implements LoaderManager.Loade
     };
     private static final int[] TO_IDS = {R.id.text1};
     private ListView listContacts;
-    private SimpleCursorAdapter cursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +29,6 @@ public class PhonesMain extends ActionBarActivity implements LoaderManager.Loade
         setContentView(R.layout.activity_phones_main);
 
         listContacts = (ListView) findViewById(R.id.listViewContacts);
-
-        //cursorAdapter = new SimpleCursorAdapter(this, R.layout.activity_phones_main, null, FROM_COLUMNS, TO_IDS, 0);
-
-        listContacts.setAdapter(cursorAdapter);
-
-
-        getLoaderManager().initLoader(0, null, this);
-
-
     }
 
 
@@ -68,78 +50,14 @@ public class PhonesMain extends ActionBarActivity implements LoaderManager.Loade
                 openSearch();
                 return true;
 
-            case R.id.action_settings:
-                openSettings();
-                return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    private void openSettings() {
-
     }
 
     private void openSearch() {
         Intent searchDialog = new Intent(this, com.sviat.k.androidphones.app.searchDialog.class);
         startActivity(searchDialog);
 
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-// If this is the loader for finding contacts in the Contacts Provider
-        // (the only one supported)
-        if (id == ContactsQuery.QUERY_ID) {
-            Uri contentUri;
-
-            // There are two types of searches, one which displays all contacts and
-            // one which filters contacts by a search query. If mSearchTerm is set
-            // then a search query has been entered and the latter should be used.
-
-            //if (mSearchTerm == null) {
-            // Since there's no search string, use the content URI that searches the entire
-            // Contacts table
-            contentUri = ContactsQuery.CONTENT_URI;
-          /*  } else {
-                // Since there's a search string, use the special content Uri that searches the
-                // Contacts table. The URI consists of a base Uri and the search string.
-                contentUri =
-                        Uri.withAppendedPath(ContactsQuery.FILTER_URI, Uri.encode(mSearchTerm));
-            }*/
-
-            // Returns a new CursorLoader for querying the Contacts table. No arguments are used
-            // for the selection clause. The search string is either encoded onto the content URI,
-            // or no contacts search string is used. The other search criteria are constants. See
-            // the ContactsQuery interface.
-            return new CursorLoader(getActivity(),
-                    contentUri,
-                    ContactsQuery.PROJECTION,
-                    ContactsQuery.SELECTION,
-                    null,
-                    ContactsQuery.SORT_ORDER);
-        }
-
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    public Context getActivity() {
-        return this;
     }
 }
