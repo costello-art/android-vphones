909324db7936879fee7512799ec2d1d9605d9989
Sviat Costigan <costello@users.noreply.github.com>
2014-10-29 21:19:45 +0200

    working version. Only shows contact list.

--- app/src/main/java/com/sviat/k/androidphones/app/PhonesMain.java
+++ app/src/main/java/com/sviat/k/androidphones/app/PhonesMain.java
@@ -1,6 +1,11 @@
 package com.sviat.k.androidphones.app;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -8,9 +13,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
-public class PhonesMain extends ActionBarActivity {
+public class PhonesMain extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String EXTRA_MESSAGE_TO_RECEIVE = "com.sviat.k.androidphones.app.message_to_send";
 
@@ -21,14 +27,24 @@ public class PhonesMain extends ActionBarActivity {
     };
     private static final int[] TO_IDS = {R.id.text1};
     private ListView listContacts;
+    private SimpleCursorAdapter cAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phones_main);
 
+        setContentView(R.layout.activity_phones_main);
         listContacts = (ListView) findViewById(R.id.listViewContacts);
+
+        cAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, null,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.CONTACT_STATUS},
+                new int[]{android.R.id.text1, android.R.id.text2}, 0);
+
+        listContacts.setAdapter(cAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
 
@@ -60,4 +76,50 @@ public class PhonesMain extends ActionBarActivity {
         startActivity(searchDialog);
 
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.  This
+        // sample only has one Loader, so we don't care about the ID.
+        // First, pick the base URI to use depending on whether we are
+        // currently filtering.
+        Uri baseUri;
+
+        baseUri = ContactsContract.Contacts.CONTENT_URI;
+
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+        return new CursorLoader(
+                this,
+                baseUri,
+                CONTACTS_SUMMARY_PROJECTION,
+                select,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cAdapter.swapCursor(null);
+    }
+
+    // These are the Contacts rows that we will retrieve.
+    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[]{
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.CONTACT_PRESENCE,
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.LOOKUP_KEY,
+    };
+
 }
